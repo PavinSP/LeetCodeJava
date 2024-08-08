21bf4913ef01b69d57ed99c4526bef6ffaba58b5
PavinSP <pavinsp122002@gmail.com>
2024-08-08 19:48:23 +0530

    commit

---
+++ src/_2Problem_1.java
@@ -0,0 +1,30 @@
+// Two pass method
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+public class _2Problem_1 {
+    public static void main(String[] args) {
+        int[] nums1 = {2, 7, 11, 15};
+        int[] nums2 = {3, 2, 4};
+        int[] nums3 = {3, 3};
+        int target1 = 9;
+        int target2 = 6;
+        int target3 = 6;
+        System.out.println(Arrays.toString(twoSum(nums1, target1)));
+        System.out.println(Arrays.toString(twoSum(nums2, target2)));
+        System.out.println(Arrays.toString(twoSum(nums3, target3)));
+    }
+    public static int[] twoSum(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++){
+            int difference = target - nums[i];
+            if (map.containsKey(difference) && map.get(difference) != i){
+                return new int[]{i, map.get(difference)};
+            }
+        }
+        return new int[]{};
+    }
+}
---
+++ src/_3Problem_1.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+// One pass method
+public class _3Problem_1 {
+    public static void main(String[] args) {
+        int[] nums1 = {2, 7, 11, 15};
+        int[] nums2 = {3, 2, 4};
+        int[] nums3 = {3, 3};
+        int target1 = 9;
+        int target2 = 6;
+        int target3 = 6;
+        System.out.println(Arrays.toString(twoSum(nums1, target1)));
+        System.out.println(Arrays.toString(twoSum(nums2, target2)));
+        System.out.println(Arrays.toString(twoSum(nums3, target3)));
+    }
+    public static int[] twoSum(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int difference = target - nums[i];
+            if (map.containsKey(difference)){
+                return new int[]{map.get(difference), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
