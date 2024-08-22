c381e856c27e8b13bd8f8f66995f1e1d76f19eae
PavinSP <pavinsp122002@gmail.com>
2024-08-22 18:41:20 +0530

    commit

---
+++ src/Problem_11.java
@@ -0,0 +1,25 @@
+// Two pointer method (watch video for clear understanding)
+public class Problem_11 {
+    public static void main(String[] args) {
+        int[] height1 = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(maxArea(height1));
+        int[] height2 = {1, 1};
+        System.out.println(maxArea(height2));
+    }
+    public static int maxArea(int[] height){
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+        while (left < right){
+            int area = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(area, maxArea);
+            if (height[left] < height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
